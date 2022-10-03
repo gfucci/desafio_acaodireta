ccb9c2dca72fdc551369dba8544df432fda9060b
gfucci <gfucci_2005@hotmail.com>
2022-10-03 12:17:24 -0300

    CSS da mensageria

--- authentication_process.php
+++ authentication_process.php
@@ -2,5 +2,7 @@
 
     require_once("helpers/db.php");
     require_once("helpers/globals.php");
+    require_once("models/Message.php");
+    require_once("dao/UserDAO.php");
 
-    
+    $userDao = new UserDAO($conn, $BASE_URL);
